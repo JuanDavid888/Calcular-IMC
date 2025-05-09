99bbd63b7b71ce7641383b25d576303aab8b46d9
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-05-09 07:09:54 -0500

    feat: :sparkles: Se agregó un mensaje dependiendo del imc
    El mensaje indica cómo estás en cuanto a tu salud en peso a comparación de tu altura

--- main.js
+++ main.js
@@ -9,6 +9,26 @@ form.addEventListener('submit', (event) => {
     const alturaValue = altura.value;
     const resultado = pesoValue / (alturaValue * alturaValue);
     const imc = resultado.toFixed(1);
+    if (imc < 18.5) {
+        document.getElementById('salud').innerHTML = 'Bajo peso';
+        document.getElementById('salud').style.color = 'red';
+        document.getElementById('salud').style.fontWeight = 'bold';
+    }
+    else if (imc >= 18.5 && imc < 25) {
+        document.getElementById('salud').innerHTML = 'Peso normal.';
+        document.getElementById('salud').style.color = 'green';
+        document.getElementById('salud').style.fontWeight = 'bold';
+    }
+    else if (imc >= 25 && imc < 30) {
+        document.getElementById('salud').innerHTML = 'Sobrepeso.';
+        document.getElementById('salud').style.color = 'orange';
+        document.getElementById('salud').style.fontWeight = 'bold';
+    }
+    else if (imc >= 30) {
+        document.getElementById('salud').innerHTML = 'Obesidad.';
+        document.getElementById('salud').style.color = 'red';
+        document.getElementById('salud').style.fontWeight = 'bold';
+    }
 
     document.getElementById('imc').innerHTML = imc;
 });
