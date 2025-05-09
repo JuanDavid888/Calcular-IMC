88bddf3d5ae8fad598b0d76683466433cace014b
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-05-09 08:38:04 -0500

    feat: :sparkles: Se agregaron consejos dependiendo su peso

--- main.js
+++ main.js
@@ -13,24 +13,34 @@ form.addEventListener('submit', (event) => {
         document.getElementById('salud').innerHTML = 'Bajo peso';
         document.getElementById('salud').style.color = 'red';
         document.getElementById('salud').style.fontWeight = 'bold';
+        
+        document.getElementById('enlace').innerHTML = '<a href="https://www.mayoclinic.org/es/healthy-lifestyle/nutrition-and-healthy-eating/expert-answers/underweight/faq-20058429" target="_blank">¿Bajo peso?</a>';
     }
     else if (imc >= 18.5 && imc < 25) {
         document.getElementById('salud').innerHTML = 'Peso normal.';
         document.getElementById('salud').style.color = 'green';
         document.getElementById('salud').style.fontWeight = 'bold';
+
+        document.getElementById('enlace').innerHTML = '<a href="https://www.minsalud.gov.co/salud/paginas/mantenga-un-peso-saludable.aspx" target="_blank">Sigue así</a>';
     }
     else if (imc >= 25 && imc < 30) {
         document.getElementById('salud').innerHTML = 'Sobrepeso.';
         document.getElementById('salud').style.color = 'orange';
         document.getElementById('salud').style.fontWeight = 'bold';
+
+        document.getElementById('enlace').innerHTML = '<a href="https://fundaciondelcorazon.com/nutricion/dieta/1350-dieta-sobrepeso.html" target="_blank">¿Sobrepeso?</a>';
     }
     else if (imc >= 30) {
         document.getElementById('salud').innerHTML = 'Obesidad.';
         document.getElementById('salud').style.color = 'red';
         document.getElementById('salud').style.fontWeight = 'bold';
+
+        document.getElementById('enlace').innerHTML = '<a href="https://www.teknon.es/es/especialidades/gonzalbez-morgaez-jose/dietetica-obesidad/recomendaciones-pacientes-obesidad" target="_blank">¿Obesidad?</a>';
     }
     
     document.getElementById('box-imc').style.display = 'block';
     document.getElementById('box-health').style.display = 'block';
+    document.getElementById('box-tips').style.display = 'block';
+
     document.getElementById('imc').innerHTML = imc;
 });
